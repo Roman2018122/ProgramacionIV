6abc3c95a538e8b8c6ca2c8e5249fde80426b6a8
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-20 18:15:16 -0500

    repòsitorio semana3

---
+++ Modulo1/dart/dart_mp/03maps_mp.dart
@@ -0,0 +1,17 @@
+void main(){
+    print("Maps");
+    final Map <String, dynamic> evento = {
+        'lugar': 'Quito',
+        'fecha' : '11/06/2024',
+        'isAlive' : true,
+        'tipo':<String>['conferencias'],
+        'images': {
+            1:'src/ditto.jpg',
+            2:'src/ditto.jpg'
+        }
+    }
+    print('El lugar del evento es: ${evento['lugar']}');
+    print('¿Está vivo? ${evento['isAlive']}');
+    print('El primer tipo es: ${evento['tipo'][0]}');
+
+}
