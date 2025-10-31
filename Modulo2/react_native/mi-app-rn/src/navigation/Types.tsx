e1adb19edca65502c61e14728c03105068b097b5
RomanTo11 <romantorresjs1118@gmail.com>
2025-10-31 09:20:48 -0500

    semana4

---
+++ Modulo2/react_native/mi-app-rn/src/navigation/Types.tsx
@@ -0,0 +1,5 @@
+
+export type RootStackParamList = {
+    TaskList: undefined;
+    TaskDetail: { id: string };
+};
---
+++ Modulo2/react_native/mi-app-rn/src/themes/Color.ts
@@ -0,0 +1,10 @@
+const colors = {
+    background: "#0d1117",
+    surface: "#161b22",
+    primary: "#2c6cb6ff",
+    text: "#c8d4e1ff",
+    subtle: "#8b949e",
+    danger: "#f85149",
+    success: "#3fb950",
+};
+export default colors;
